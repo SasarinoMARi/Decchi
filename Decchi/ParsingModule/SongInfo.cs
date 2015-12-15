829159abb1aec52447b86d919d372124b420347d
Sasarino MARi <tjddlf2645@gmail.com>
2015-12-15 17:20:49 +0900

    SongInfo 문자열 파싱 심화
    이제 {가나다/Title/라마바사} 와 같이 포맷을 적용하면 제목이 null일 경우 "가나다"와 "라마바사"도 결과 문자열에서
    제외시킵니다.

--- Decchi/ParsingModule/SongInfo.cs
+++ Decchi/ParsingModule/SongInfo.cs
@@ -1,4 +1,6 @@
-﻿namespace ParsingModule
+﻿using System.Text.RegularExpressions;
+
+namespace ParsingModule
 {
 	public abstract class SongInfo
 	{
@@ -6,28 +8,49 @@
 
 		protected abstract string Client { get; }
 
-		public string	Title	{ get; protected set; }
-		public string	Album	{ get; protected set; }
-		public string	Artist	{ get; protected set; }
-		public bool		Loaded	{ get; protected set; }
+		public string Title { get; protected set; }
+		public string Album { get; protected set; }
+		public string Artist { get; protected set; }
+		public bool Loaded { get; protected set; }
 
-		public abstract bool GetCurrentPlayingSong();
+		public abstract bool GetCurrentPlayingSong( );
 
-		public const string defaultFormat = "/artist/ - /title/ via (/client/)";
-		public override string ToString()
+		public const string defaultFormat = "{/Artist/의 }{/Title/을 }듣고 있어요! {/Via/} - {/Client/}";
+		public override string ToString( )
 		{
-			return ToString(defaultFormat);
+			return ToString( defaultFormat );
 		}
-		public string ToString(string format)
+		public string ToString( string format )
 		{
-			string str = format
-			.Replace("/title/", this.Title)
-			.Replace("/artist/", this.Artist)
-			.Replace("/album/", this.Album)
-			.Replace("/client/", this.Client)
-			.Replace("/via/", SongInfo.Via);
+			string str = format;
+
+			str = Replace( str, "Title", this.Title );
+			str = Replace( str, "Artist", this.Artist );
+			str = Replace( str, "Album", this.Album );
+			str = Replace( str, "Client", this.Client );
+			str = Replace( str, "Via", SongInfo.Via );
 
 			return str;
+		}
+
+		private string Replace( string SourceString, string TargetString, string Value )
+		{
+			string replaceString = string.Empty;
+			var regex = new Regex( @"{[^{]*/" + TargetString + "/[^}]*}" );
+			var match = regex.Match(SourceString);
+			if ( match.Success )
+			{
+				if ( !string.IsNullOrEmpty(Value) )
+				{
+					replaceString = match.Value.Replace( "{", "" ).Replace( "}", "" ).Replace( "/" + TargetString + "/", Value );
+				}
+				else
+				{
+					replaceString = string.Empty;
+				}
+				SourceString = regex.Replace( SourceString, replaceString );
+			}
+			return SourceString;
 		}
 	}
 }
