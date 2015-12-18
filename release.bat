ed8156b91293acad26a74e7d4bec3a0d1fc85359
RyuaNerin <admin@ryuanerin.kr>
2015-12-18 10:47:21 +0900

    스크립트 조금 수정

--- release.bat
+++ release.bat
@@ -1,14 +1,12 @@
 @echo off
 
-IF "%~1"=="Release" (
-	IF EXIST "%~2ConfuserEx\Confuser.CLI.exe" (
-		echo ^<project outputDir="%~3Confused" baseDir="%~3" xmlns="http://confuser.codeplex.com"^>	>  "%~3confuserex.crproj"
-		echo ^<rule pattern="true" inherit="false"^>												>> "%~3confuserex.crproj"
-		echo ^<protection id="resources"/^>															>> "%~3confuserex.crproj"
-		echo ^</rule^>																				>> "%~3confuserex.crproj"
-		echo ^<module path="%~4"/^>																	>> "%~3confuserex.crproj"
-		echo ^</project^>																			>> "%~3confuserex.crproj"
+IF EXIST "%~2ConfuserEx\Confuser.CLI.exe" (
+	echo ^<project outputDir="%~3Confused" baseDir="%~3" xmlns="http://confuser.codeplex.com"^>	>  "%~3confuserex.crproj"
+	echo ^<rule pattern="true" inherit="false"^>												>> "%~3confuserex.crproj"
+	echo ^<protection id="resources"/^>															>> "%~3confuserex.crproj"
+	echo ^</rule^>																				>> "%~3confuserex.crproj"
+	echo ^<module path="%~4"/^>																	>> "%~3confuserex.crproj"
+	echo ^</project^>																			>> "%~3confuserex.crproj"
 
-		call "%~2ConfuserEx\Confuser.CLI.exe" -n "%~3confuserex.crproj"
-	)
+	call "%~2ConfuserEx\Confuser.CLI.exe" -n "%~3confuserex.crproj"
 )
