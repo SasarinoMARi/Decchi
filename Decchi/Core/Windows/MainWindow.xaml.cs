420a9da1b2ad775a6a571cafa057001657e32116
Sasarino MARi <tjddlf2645@gmail.com>
2016-01-15 13:32:44 +0900

    reslove #23

--- Decchi/Core/Windows/MainWindow.xaml.cs
+++ Decchi/Core/Windows/MainWindow.xaml.cs
@@ -26,7 +26,9 @@ namespace Decchi.Core.Windows
         private Storyboard m_toNormal;
         private string     m_newUrl;
 
-        public MainWindow( )
+		private bool forceQuitFlag = false; // 트레이 옵션 여부 상관없이 종료
+
+		public MainWindow( )
         {
             MainWindow.Instance = this;
 
@@ -146,7 +148,9 @@ namespace Decchi.Core.Windows
 
             Process.Start(new ProcessStartInfo("cmd.exe", "/c " + batchPath) { CreateNoWindow = true, UseShellExecute = false, WindowStyle = ProcessWindowStyle.Hidden });
             Application.Current.Shutdown();
-            this.Close();
+
+			forceQuitFlag = true;
+			this.Close();
         }
 
         private void ctlHomepage_Click(object sender, RoutedEventArgs e)
@@ -172,7 +176,7 @@ namespace Decchi.Core.Windows
 
         private void MetroWindow_Closing(object sender, CancelEventArgs e)
         {
-            if (Globals.Instance.TrayWhenClose)
+            if (Globals.Instance.TrayWhenClose && !forceQuitFlag )
             {
                 this.GoTray();
                 e.Cancel = true;
@@ -261,7 +265,8 @@ namespace Decchi.Core.Windows
                 {
                     await this.ShowMessageAsync("X(", "트위터에 로그인 하지 못했어요");
 
-                    this.Close();
+					forceQuitFlag = true;
+					this.Close();
 
                     return;
                 }
@@ -274,6 +279,7 @@ namespace Decchi.Core.Windows
                 {
                     await this.ShowMessageAsync("X(", "트위터에 로그인 하지 못했어요");
 
+					forceQuitFlag = true;
                     this.Close();
 
                     return;
@@ -299,7 +305,8 @@ namespace Decchi.Core.Windows
                 Globals.Instance.TwitterSecret = null;
                 Globals.Instance.SaveSettings();
 
-                this.Close( );
+				forceQuitFlag = true;
+				this.Close( );
 
                 return;
             }
@@ -327,7 +334,8 @@ namespace Decchi.Core.Windows
             {
                 await this.ShowMessageAsync("X(", "서버에 연결하지 못했어요");
 
-                this.Close();
+				forceQuitFlag = true;
+				this.Close();
 
                 return;
             }
