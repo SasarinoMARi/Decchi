4c41503f162c0625f7b5a3b11c0536fe5c9d317d
RyuaNerin <admin@ryuanerin.kr>
2016-01-15 12:01:05 +0900

    Remove Confuser.

--- build.bat
+++ build.bat
@@ -6,20 +6,6 @@ if not exist %MSBUILD% set MSBUILD="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild
 if not exist %MSBUILD% set MSBUILD="%ProgramFiles%\MSBuild\14.0\Bin\MSBuild.exe"
 if not exist %MSBUILD% echo Error: Could not find MSBuild.exe. && goto :eof
 
-
-SET CDIR=%cd%\Decchi\bin\Confused\
-SET RDIR=%cd%\Decchi\bin\Release\
-
 del /F /S /Q "Decchi\bin\Release"
-del /F /S /Q "Decchi\bin\Confused"
 
 %MSBUILD% "%cd%\Decchi\Decchi.csproj" /p:Configuration=Release
-
-echo ^<project outputDir="%CDIR%" baseDir="%RDIR%" xmlns="http://confuser.codeplex.com"^>	>  "%RDIR%ConfuserEx.crproj"
-echo ^<rule pattern="true" inherit="false"^>												>> "%RDIR%ConfuserEx.crproj"
-echo ^<protection id="resources"/^>															>> "%RDIR%ConfuserEx.crproj"
-echo ^</rule^>																				>> "%RDIR%ConfuserEx.crproj"
-echo ^<module path="Decchi.exe" snKey="%cd%\Decchi\Uncommit\decchi.snk" /^>					>> "%RDIR%ConfuserEx.crproj"
-echo ^</project^>																			>> "%RDIR%ConfuserEx.crproj"
-
-"%cd%\ConfuserEx\Confuser.CLI.exe" -n "%RDIR%confuserex.crproj"
